adbba3e97b59b163c17cd6595ac819cbc99954c3
skidrow8852 <amine12solo@gmail.com>
2025-01-15 16:40:06 +0100

    Added files upload to AWS S3

---
+++ src/aws.ts
@@ -0,0 +1,18 @@
+import { S3 } from "aws-sdk";
+import fs from "fs";
+
+const s3 = new S3({
+    accessKeyId : process.env.ACCESS_KEY_ID,
+    secretAccessKey : process.env.SECRET_ACCESS_KEY,
+    endpoint: process.env.ENDPOINT
+})
+
+
+export const uploadFile = async (fileName:string, localFilePath : string) => {
+    const fileContent = fs.readFileSync(localFilePath);
+    const response = await s3.upload({
+        Body : fileContent,
+        Bucket: "vercel",
+        Key : fileName
+    }).promise()
+}
