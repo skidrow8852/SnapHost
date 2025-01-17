fc94bee7647a0e8fc12a62f299060228b744a409
skidrow8852 <amine12solo@gmail.com>
2025-01-17 01:55:11 +0100

    updated Metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,29 +7,20 @@ export const metadata: Metadata = {
   title: "SnapHost - Clone and Deploy Your Projects with Ease",
   description: "SnapHost allows you to clone your GitHub repository and deploy it with minimal setup. Support for static websites and React projects. Effortless hosting and deployment.",
   icons: [
-    { rel: "icon", href: "/favicon.ico", url : "https://example.com/icon.png" },
+    { rel: "icon", href: "/favicon.ico", url : "https://snaphost.co/favicon.ico" },
    
   ],
   openGraph: {
     title: "SnapHost - Clone and Deploy Your Projects with Ease",
     description: "Clone your GitHub repository and deploy static websites or React projects effortlessly with SnapHost. Minimal setup, fast deployment.",
-    url: "https://www.snaphost.com",
+    url: "https://snaphost.co",
     siteName: "SnapHost",
-    images: [
-      {
-        url: "/og-image.png",
-        width: 1200,
-        height: 630,
-        alt: "SnapHost - Effortless GitHub Repo Cloning and Deployment"
-      }
-    ],
   },
   twitter: {
     card: "summary_large_image",
     site: "@SnapHost",
     title: "SnapHost - Clone and Deploy Your Projects with Ease",
     description: "Effortlessly clone and deploy your GitHub projects. Fast and reliable hosting for static websites and React apps.",
-    images: "/og-image.png"
   },
   robots: "index, follow",
 };
