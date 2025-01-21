6a8e9abbf579b397e3f62c6d688046f2a5e8f63e
skidrow8852 <amine12solo@gmail.com>
2025-01-21 12:50:28 +0100

    Added Oauth

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
@@ -33,6 +34,8 @@ export function LoginForm({
   } = useForm<SigninFormInputs>();
 
   const [isLoading, setIsLoading] = React.useState<boolean>(false);
+  const [isGithubLoading, setIsGithubLoading] = React.useState<boolean>(false);
+  const [isGoogleLoading, setIsGoogleLoading] = React.useState<boolean>(false);
   const { toast } = useToast();
 
   // Form submit handler
@@ -96,6 +99,48 @@ const onSubmit: SubmitHandler<SigninFormInputs> = async (values) => {
   }
 };
 
+
+const signInWithGithub = async () => {
+  try{
+
+    setIsGithubLoading(true)
+     await authClient.signIn.social({
+          provider: "github",
+          callbackURL : "/dashboard"
+      })
+   
+  }catch(e){
+      toast({
+              title: "An Error Occured.",
+              description: "Please try again later.",
+              variant: "destructive",
+            });
+  }finally{
+     setIsGithubLoading(false)
+  }
+}
+
+const signInWithGoogle = async () => {
+    try{
+
+    setIsGoogleLoading(true)
+     await authClient.signIn.social({
+          provider: "google",
+          callbackURL : "/dashboard"
+      })
+   
+  }catch(e){
+      toast({
+              title: "An Error Occured.",
+              description: "Please try again later.",
+              variant: "destructive",
+            });
+  }finally{
+     setIsGoogleLoading(false)
+  }
+
+}
+
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card>
@@ -106,10 +151,10 @@ const onSubmit: SubmitHandler<SigninFormInputs> = async (values) => {
           </CardDescription>
         </CardHeader>
         <CardContent>
-          <form onSubmit={handleSubmit(onSubmit)}>
+          
             <div className="grid gap-6">
               <div className="flex flex-col gap-4">
-                <Button variant="outline" className="w-full">
+                <Button disabled={isGithubLoading} onClick={signInWithGithub} variant="outline" className="w-full">
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                     <path
                       d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"
@@ -118,7 +163,7 @@ const onSubmit: SubmitHandler<SigninFormInputs> = async (values) => {
                   </svg>
                   Login with Github
                 </Button>
-                <Button variant="outline" className="w-full">
+                <Button disabled={isGoogleLoading} onClick={signInWithGoogle} variant="outline" className="w-full">
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                     <path
                       d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
@@ -133,6 +178,7 @@ const onSubmit: SubmitHandler<SigninFormInputs> = async (values) => {
                   Or continue with
                 </span>
               </div>
+            <form onSubmit={handleSubmit(onSubmit)}>
               <div className="grid gap-6">
                 <div className="grid gap-2">
                   <Label htmlFor="email">Email</Label>
@@ -175,14 +221,15 @@ const onSubmit: SubmitHandler<SigninFormInputs> = async (values) => {
                   Login
                 </Button>
               </div>
-              <div className="text-center text-sm">
+              <div className="text-center text-sm pt-6">
                 Don&apos;t have an account?{" "}
                 <Link href="/register" className="underline underline-offset-4">
                   Sign up
                 </Link>
               </div>
+              </form>
             </div>
-          </form>
+          
         </CardContent>
       </Card>
       <div className="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 [&_a]:hover:text-primary">
--- src/components/sigup-form.tsx
+++ src/components/sigup-form.tsx
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 "use client";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
@@ -30,6 +33,8 @@ export function SignupForm({
   } = useForm<SignupFormInputs>();
 
   const [isLoading, setIsLoading] = React.useState<boolean>(false);
+  const [isGithubLoading, setIsGithubLoading] = React.useState<boolean>(false);
+  const [isGoogleLoading, setIsGoogleLoading] = React.useState<boolean>(false);
   const { toast } = useToast();
 
   const onSubmit: SubmitHandler<SignupFormInputs> = async (values) => {
@@ -46,10 +51,36 @@ export function SignupForm({
         },
         {
           onRequest: () => setIsLoading(true),
-          onSuccess: () => setIsLoading(false),
+           onSuccess: async (ctx) => {
+          
+          const userId = ctx.data.user.id as string;
+          const response = await fetch("/api/token", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ userId }),
+          });
+
+          if (response.ok) {
+            const data = await response.json();
+            const authToken = data.authToken;
+
+            if (authToken) {
+              localStorage.setItem("bearer_token", authToken as string);
+            }
+            setIsLoading(false);
+          } else {
+          
+            toast({
+              title: "Token generation failed.",
+              description: "Please try again later.",
+              variant: "destructive",
+            });
+          }
+        },
           onError: (ctx) => {
             setIsLoading(false);
-            
             setError("email", {
               type: "manual",
               message: ctx.error.message,
@@ -72,36 +103,77 @@ export function SignupForm({
     }
   };
 
+  const signInWithGithub = async () => {
+    try{
+  
+      setIsGithubLoading(true)
+       await authClient.signIn.social({
+            provider: "github",
+            callbackURL : "/dashboard"
+        })
+     
+    }catch(e){
+        toast({
+                title: "An Error Occured.",
+                description: "Please try again later.",
+                variant: "destructive",
+              });
+    }finally{
+       setIsGithubLoading(false)
+    }
+  }
+  
+  const signInWithGoogle = async () => {
+      try{
+  
+      setIsGoogleLoading(true)
+       await authClient.signIn.social({
+            provider: "google",
+            callbackURL : "/dashboard"
+        })
+     
+    }catch(e){
+        toast({
+                title: "An Error Occured.",
+                description: "Please try again later.",
+                variant: "destructive",
+              });
+    }finally{
+       setIsGoogleLoading(false)
+    }
+  
+  }
+
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card>
         <CardHeader className="text-center">
           <CardTitle className="text-xl">Welcome</CardTitle>
           <CardDescription>
-            Signup with your Github or Google account
+            Sign up with your Github or Google account
           </CardDescription>
         </CardHeader>
         <CardContent>
-          <form onSubmit={handleSubmit(onSubmit)}>
+          
             <div className="grid gap-6">
               <div className="flex flex-col gap-4">
-                <Button variant="outline" className="w-full">
+                <Button disabled={isGithubLoading} onClick={signInWithGithub} variant="outline" className="w-full">
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                     <path
                       d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"
                       fill="currentColor"
                     />
                   </svg>
-                  Signup with Github
+                  Sign up with Github
                 </Button>
-                <Button variant="outline" className="w-full">
+                <Button disabled={isGoogleLoading} onClick={signInWithGoogle} variant="outline" className="w-full">
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                     <path
                       d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
                       fill="currentColor"
                     />
                   </svg>
-                  Signup with Google
+                  Sign up with Google
                 </Button>
               </div>
               <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
@@ -109,56 +181,58 @@ export function SignupForm({
                   Or continue with
                 </span>
               </div>
-              <div className="grid gap-6">
-                <div className="grid gap-2">
-                  <Label htmlFor="email">Name</Label>
-                  <Input
-                    id="name"
-                    type="name"
-                    placeholder="John Doe"
-                    {...register("name", { required: "Name is required" })}
-                  />
-                  {errors.name && (
-                    <span className="text-sm text-red-500">
-                      {errors.name.message}
-                    </span>
-                  )}
-                </div>
-                <div className="grid gap-2">
-                  <Label htmlFor="email">Email</Label>
-                  <Input
-                    id="email"
-                    type="email"
-                    placeholder="example@example.com"
-                    {...register("email", { required: "Email is required" })}
-                  />
-                  {errors.email && (
-                    <span className="text-sm text-red-500">
-                      {errors.email.message}
-                    </span>
-                  )}
-                </div>
-                <div className="grid gap-2">
-                  <div className="flex items-center">
-                    <Label htmlFor="password">Password</Label>
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="grid gap-6">
+                  <div className="grid gap-2">
+                    <Label htmlFor="email">Name</Label>
+                    <Input
+                      id="name"
+                      type="name"
+                      placeholder="John Doe"
+                      {...register("name", { required: "Name is required" })}
+                    />
+                    {errors.name && (
+                      <span className="text-sm text-red-500">
+                        {errors.name.message}
+                      </span>
+                    )}
                   </div>
-                  <Input
-                    id="password"
-                    type="password"
-                    {...register("password", {
-                      required: "Password is required",
-                    })}
-                  />
-                  {errors.password && (
-                    <span className="text-sm text-red-500">
-                      {errors.password.message}
-                    </span>
-                  )}
+                  <div className="grid gap-2">
+                    <Label htmlFor="email">Email</Label>
+                    <Input
+                      id="email"
+                      type="email"
+                      placeholder="example@example.com"
+                      {...register("email", { required: "Email is required" })}
+                    />
+                    {errors.email && (
+                      <span className="text-sm text-red-500">
+                        {errors.email.message}
+                      </span>
+                    )}
+                  </div>
+                  <div className="grid gap-2">
+                    <div className="flex items-center">
+                      <Label htmlFor="password">Password</Label>
+                    </div>
+                    <Input
+                      id="password"
+                      type="password"
+                      {...register("password", {
+                        required: "Password is required",
+                      })}
+                    />
+                    {errors.password && (
+                      <span className="text-sm text-red-500">
+                        {errors.password.message}
+                      </span>
+                    )}
+                  </div>
+                  <Button disabled={isLoading} type="submit" className="w-full">
+                    Sign up
+                  </Button>
                 </div>
-                <Button disabled={isLoading} type="submit" className="w-full">
-                  Signup
-                </Button>
-              </div>
+              </form>
               <div className="text-center text-sm">
                 Already have an account?{" "}
                 <Link href="/login" className="underline underline-offset-4">
@@ -166,7 +240,7 @@ export function SignupForm({
                 </Link>
               </div>
             </div>
-          </form>
+          
         </CardContent>
       </Card>
       <div className="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 [&_a]:hover:text-primary">
